7b2d94085310fe6f59b478e1eded231f1126a259
Yang Liu <liuyang12@focusmedia.cn>
2019-09-09 17:27:04 +0800

    update

--- utils.py
+++ utils.py
@@ -151,7 +151,7 @@ def load_wav_to_torch(full_path):
 
 def load_filepaths_and_text(filename, split=" "):
     with open(filename, encoding='utf-8') as f:
-        filepaths_and_text = [line.strip().split(split, 2) for line in f]
+        filepaths_and_text = [line.strip()[3:].split(split, 2) for line in f]
     return filepaths_and_text
 
 
